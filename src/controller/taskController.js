30f02fa6d4571d476a44616579215613e19fbff1
wtfrodriguu1 <142605628+wtfrodriguu1@users.noreply.github.com>
2024-04-15 12:19:27 -0300

    Add files via upload

---
+++ src/controller/taskController.js
@@ -0,0 +1,36 @@
+const connection = require('../config/db');
+const dotenv = require('dotenv').config();
+
+async function storeTask(request, response) {
+    const params = Array(
+        request.body.title,
+        request.body.description
+    );
+
+    const query = "INSERT INTO tasks(title,description) VALUES(?,?)";
+
+    connection.query(query, params, (err, results) => {
+        if(results) {
+            response
+                .status(201)
+                .json({
+                    sucess: true, 
+                    message: "Sucesso!",
+                    data: results
+                })
+        } else{
+            response
+                .status(400)
+                .json({
+                    sucess: false,
+                    message: "ops, deu problema!",
+                    data: err
+                })
+        }
+    })
+
+}
+
+module.exports = {
+    storeTask
+}
